9f204f97b5c974552476a666f238c4c73e985158
Ashar <ekicenx@gmail.com>
2018-05-27 20:42:47 +0700

    Add function for craft

---
+++ app/controllers/api/v1/crafts_controller.rb
@@ -0,0 +1,48 @@
+module Api
+  module V1
+    class CraftsController < ApplicationController
+    
+      def index
+        crafts = Craft.all
+        render json: crafts
+      end
+
+      def show
+        craft = Craft.find(params[:id])
+        render json: craft
+      end
+
+      def create
+        craft = Craft.new(craft_params)
+
+        if craft.save
+          render json: {status: "Success", message: "craft Successfully Created", data: craft}, status: :ok
+        else
+          render json: {status: "Error", message: "craft Can't Be Saved", data: craft.errors}, status: :unprocessable_entity
+        end
+      end
+
+      def update
+        craft = Craft.find(params[:id])
+
+        if craft.update_attributes(craft_params)
+          render json: {status: "Success", message: "craft Successfully Updated", data: craft}, status: :ok
+        else
+          render json: {status: "Error", message: "craft Can't Be Saved", data: craft.errors}, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        craft = Craft.find(params[:id])
+        craft.destroy
+        render json: {status: "Success", message: "Deleted ", data: craft}, status: :ok
+      end
+
+      private
+        def craft_params
+          params.require(:craft).permit(:name, :description)
+        end
+
+    end
+  end
+end
