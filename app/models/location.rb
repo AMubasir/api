cda5582afc0774d176a1b15a56b454d6cee698df
Ashar <ekicenx@gmail.com>
2018-05-27 22:11:52 +0700

    validation failed

--- app/models/location.rb
+++ app/models/location.rb
@@ -4,8 +4,8 @@ class Location < ApplicationRecord
   has_many :crafts
 
   validates :address, presence: true
-  validates :latitude, presence: true
-  validates :longitude, presence: true
+  # validates :latitude, presence: true
+  # validates :longitude, presence: true
 
   geocoded_by :address
   after_validation :geocode, :if => :address_changed?
