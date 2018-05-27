b6cd61d8cb710326a4b84bfbf37cb9a77ab44d73
Ashar <ekicenx@gmail.com>
2018-05-27 14:47:54 +0700

    Secret-key-base

--- config/initializers/knock.rb
+++ config/initializers/knock.rb
@@ -12,7 +12,7 @@ Knock.setup do |config|
   config.token_lifetime = 1.week
   config.token_signature_algorithm = 'HS256'
   config.token_secret_signature_key = -> { Rails.application.credentials.read }
-  config.token_secret_signature_key = -> { Rails.application.credentials.fetch(:secret_key_base) }
+  # config.token_secret_signature_key = -> { Rails.application.credentials.fetch(:secret_key_base) }
   # config.token_secret_signature_key = -> { Rails.application.secrets.secret_key_base }
   config.not_found_exception_class_name = 'ActiveRecord::RecordNotFound'
 
