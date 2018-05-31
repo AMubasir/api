3e2908e1c74442843c3140d1ff7355a11e8c836e
Ashar <ekicenx@gmail.com>
2018-05-31 08:57:06 +0700

    Migration Error 2

--- db/migrate/20180529071202_remove_location_from_craft.rb
+++ db/migrate/20180529071202_remove_location_from_craft.rb
@@ -1,5 +1,5 @@
 class RemoveLocationFromCraft < ActiveRecord::Migration[5.2]
   def change
-    remove_reference :crafts, :location, foreign_key: true
+    # remove_reference :crafts, :location, foreign_key: true
   end
 end
